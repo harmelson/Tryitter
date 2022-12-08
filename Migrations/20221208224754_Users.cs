a5df8cfbe66310c3e69d0e3b23cf04d27e763aff
calilisantos <calilisantos@gmail.com>
2022-12-08 19:56:28 -0300

    feat(migrations): database migrations

---
+++ Migrations/20221208224754_Users.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tryitter.Migrations
+{
+    /// <inheritdoc />
+    public partial class Users : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    IdUser = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmailUser = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NameUser = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.IdUser);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PostUser",
+                columns: table => new
+                {
+                    IdPostUser = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdUser = table.Column<int>(type: "int", nullable: false),
+                    IdPost = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostUser", x => x.IdPostUser);
+                    table.ForeignKey(
+                        name: "FK_PostUser_Users_IdUser",
+                        column: x => x.IdUser,
+                        principalTable: "Users",
+                        principalColumn: "IdUser",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    IdPost = table.Column<int>(type: "int", nullable: false),
+                    MessagePost = table.Column<string>(type: "nvarchar(280)", maxLength: 280, nullable: false),
+                    LikesPost = table.Column<int>(type: "int", nullable: false),
+                    SharesPost = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.IdPost);
+                    table.ForeignKey(
+                        name: "FK_Post_PostUser_IdPost",
+                        column: x => x.IdPost,
+                        principalTable: "PostUser",
+                        principalColumn: "IdPostUser",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostUser_IdUser",
+                table: "PostUser",
+                column: "IdUser");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Post");
+
+            migrationBuilder.DropTable(
+                name: "PostUser");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
