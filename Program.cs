b35e611e2a9844bd113b15060c1b6826e9450e8c
calilisantos <calilisantos@gmail.com>
2022-12-09 07:52:24 -0300

    feat(Program): implement AddMvcCore method

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers()
                 .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddMvcCore();                
 builder.Services.AddDbContext<MyContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
