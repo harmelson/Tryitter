09068e7a00e1a151a415ae39af0f711c3dd6edde
calilisantos <calilisantos@gmail.com>
2022-12-09 10:30:41 -0300

    feat(User): add PUT route

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,7 +8,6 @@ namespace Tryitter.Controllers
 {
   [ApiController]
   [Route("user")]
-  [AllowAnonymous]
   public class UserController : Controller
   {
     private readonly UserRepository _repository;
@@ -18,6 +17,7 @@ namespace Tryitter.Controllers
     }
 
     [HttpPost]
+    [AllowAnonymous]
     public IActionResult Add([FromBody] User user)
     {
       Regex regex = new(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
@@ -32,6 +32,23 @@ namespace Tryitter.Controllers
       if (user.Password.Length < 8) return BadRequest("Password must have at least 8 characters");
 
       return Created("",_repository.AddUser(user));
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update([FromBody] User user, int id)
+    {
+      Regex regex = new(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+      if (user.EmailUser == null || user.NameUser == null || user.Password == null)
+      {
+        return BadRequest("All fields must be passed");
+      }
+
+      if (!regex.IsMatch(user.EmailUser)) return BadRequest("EmailUser must be valid");
+      if (user.EmailUser.Length < 8) return BadRequest("EmailUser must have at least 8 characters");
+
+      if (user.Password.Length < 8) return BadRequest("Password must have at least 8 characters");
+
+      return Ok(_repository.UpdateUser(user, id));
     }
   }
 }
