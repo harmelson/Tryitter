419daebc3ab4a43eff41fbcd4fcb4a996e2070da
calilisantos <calilisantos@gmail.com>
2022-12-09 09:50:57 -0300

    feat(User): add annotations in model

--- Models/User.cs
+++ Models/User.cs
@@ -2,14 +2,15 @@ namespace Tryitter.Models
 {
   using System.ComponentModel.DataAnnotations;
   using System.ComponentModel.DataAnnotations.Schema;
+  using System.Text.Json.Serialization;
   public class User
   {
-    [Key]
+    [Key, JsonIgnore]
     public int IdUser { get; set; }
     public string EmailUser  { get; set; } = null!;
     public string NameUser  { get; set; } = null!;
     public string Password  { get; set; } = null!;
-    [InverseProperty("User")]
-    public ICollection<PostUser> PostUsers  { get; set; } = null!;
+    [InverseProperty("User"), JsonIgnore]
+    public ICollection<PostUser>? PostUsers  { get; set; } = null!;
   }
 }
