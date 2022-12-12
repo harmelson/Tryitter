89d013fb603419a5a7a68c1c0c1302d76c2caca2
harmelson <gabriel.harmel@hotmail.com>
2022-12-12 02:55:23 -0300

    fix(models): fix plural names according to the tables

--- Models/Post.cs
+++ Models/Post.cs
@@ -5,14 +5,15 @@ namespace Tryitter.Models
   using System.Text.Json.Serialization;
   public class Post
   {
-    [Key, JsonIgnore]
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
     public int IdPost { get; set; }
     [MinLength(1), MaxLength(280)]
     public string MessagePost  { get; set; } = null!;
     public int LikesPost  { get; set; }
     public int SharesPost  { get; set; }
     [InverseProperty("Post"), JsonIgnore]
-    public PostUser PostUsers  { get; set; } = null!;
+    public PostUser PostUser  { get; set; } = null!;
   }
 
   public class PostDTO
