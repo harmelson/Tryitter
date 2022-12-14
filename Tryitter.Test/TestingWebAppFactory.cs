deb26ab9aea426ecc6e4fbff7c8cd7080caa0198
calilisantos <calilisantos@gmail.com>
2022-12-14 01:05:47 -0300

    refactor(Tryitter.Test): reshape directories

---
+++ Tryitter.Test/TestingWebAppFactory.cs
@@ -0,0 +1,15 @@
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.AspNetCore.Mvc.Testing;
+// using Microsoft.Extensions.Hosting;
+
+// public class TestingWebAppFactory<TEntryPoint> : WebApplicationFactory<Program> where TEntryPoint : Program
+// {
+//   protected override void ConfigureWebHost(IWebHostBuilder builder)
+//   {
+//     builder.UseEnvironment("Test");
+
+//     base.ConfigureWebHost(builder);
+//   }
+
+//   protected override IHost CreateHost(IHostBuilder builder) => builder.CreateHost(builder);
+// }
