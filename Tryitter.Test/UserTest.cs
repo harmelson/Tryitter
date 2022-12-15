a92cd275043db04f6cc20efa0f96c5ceed6ffc9c
harmelson <gabriel.harmel@hotmail.com>
2022-12-14 23:51:15 -0300

    tests: kabum

--- Tryitter.Test/UserTest.cs
+++ Tryitter.Test/UserTest.cs
@@ -6,6 +6,8 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Tryitter.Models;
 using Tryitter.Services;
+using System.Text.Json;
+using System.Text;
 
 namespace Tryitter.Test;
 public class TestUser : IClassFixture<WebApplicationFactory<Program>>
@@ -17,17 +19,44 @@ public class TestUser : IClassFixture<WebApplicationFactory<Program>>
       _factory = factory;
     }
   
+  [Theory(DisplayName = "POST /login deve responder com status 200 - 299 para rotas")]
+  [InlineData("/login", "billg@net.com", "unclebill", 1, "12345678")]
+  public async Task LoginEndpointReturnSuccess(string url, string emailUser, string nameUser, int id, string password)
+  {
+    string token = new TokenGenerator().Generate(new User { EmailUser = emailUser, IdUser = id, NameUser = nameUser });
+    var client = _factory.CreateClient();
+
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    var response = await client.PostAsync(url, new StringContent(JsonSerializer.Serialize(new UserDTO { EmailUser = emailUser, Password = password }), Encoding.UTF8, "application/json"));
+  
+    response.EnsureSuccessStatusCode();
+  }
+
   [Theory(DisplayName = "POST /user deve responder com status 200 - 299 para rotas")]
-  [InlineData("/login", "billg@net.com", "unclebill", "12345678")]
+  [InlineData("/user", "billg@net.com", "unclebill", "12345678")]
   public async Task PostEndpointReturnSuccess(string url, string emailUser, string nameUser, string password)
   {
-    User user = new() {EmailUser =  emailUser, NameUser = nameUser, Password = password};
-
-    string token = new TokenGenerator().Generate(user);
     var client = _factory.CreateClient();
-    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-    var response = await client.GetAsync(url);
 
+    var response = await client.PostAsync(url, new StringContent(JsonSerializer.Serialize(new User { EmailUser = emailUser, NameUser = nameUser, Password = password }), Encoding.UTF8, "application/json"));
+  
+    response.EnsureSuccessStatusCode();
+  }
+
+  [Theory(DisplayName = "DELETE /user/{id} deve responder com status 200 - 299 para rotas")]
+  [InlineData("/user", "bilaaaaaalg@net.com", "unclebill", "12345678", 10)]
+  public async Task DeleteEndpointReturnSuccess(string url, string emailUser, string nameUser, string password, int id)
+  {
+    User user = new() {EmailUser = emailUser, NameUser = nameUser, Password = password};
+    user.IdUser = id;
+
+    string token = new TokenGenerator().Generate(new User { EmailUser = emailUser, IdUser = user.IdUser, NameUser = nameUser });
+    var client = _factory.CreateClient();
+
+  
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    var response = await client.DeleteAsync($"{url}/{id}");
+  
     response.EnsureSuccessStatusCode();
   }
 }
